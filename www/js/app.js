62d9d8914c971359bd01e173b32351e29a916e4a
James Long <jlongster@jlongster.com>
2012-07-31 15:20:57 -0400

    initial

---
+++ www/js/app.js
@@ -0,0 +1,13 @@
+
+require.config({ 
+    baseUrl: 'js/lib',
+    paths: {'jquery':
+            ['//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min',
+             'lib/jquery']}
+});
+
+define("app", function(require) {
+    var $ = require('jquery');
+
+    
+});
